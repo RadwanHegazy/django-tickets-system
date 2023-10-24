3df89cefb7761ac43870bc1aada52c7ede7d7a0c
RadwanHegazy <radwangaber22@gmail.com>
2023-10-24 14:47:38 +0300

    ticket app code

---
+++ ticket_app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Event, Event_Image, Visitor
+
+class EventPanel ( admin.ModelAdmin ) :
+    list_display = ['name','user','start_at']
+admin.site.register(Event, EventPanel)
+
+
+class VisitorPanel ( admin.ModelAdmin ) :
+    list_display = ['full_name','event','is_arrived']
+admin.site.register(Visitor, VisitorPanel)
+
