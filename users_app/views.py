33a0fff333b5b31701baae7c3d87651a0e3c1f76
RadwanHegazy <radwangaber22@gmail.com>
2023-10-25 21:44:26 +0300

    add visitor registration view

--- users_app/views.py
+++ users_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
+from ticket_app.models import Event, Event_Image, Visitor
 from users_app.models import User
 from django.contrib.auth.views import auth_login
 
@@ -44,3 +45,39 @@ def register(request) :
     return render(request,'register.html')
 
 
+
+def visitor_register (request, eventuuid) : 
+    event = get_object_or_404(Event,uuid=eventuuid)
+    event_images = Event_Image.objects.filter(event=event)
+
+
+    context = {
+        'event' : event
+    }
+
+    if event_images.exists() : 
+        context['images'] = '@'.join([i.image.url for i in event_images])
+
+    if request.method == 'POST' : 
+        full_name = request.POST['full_name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+
+        visitor = Visitor.objects.create(
+            full_name = full_name,
+            email = email,
+            phone = phone,
+            event = event
+        )
+
+        if 'img' in request.FILES :
+            img = request.FILES['img']
+            visitor.picture = img
+        
+        visitor.save()
+
+        context['msg'] = 'تم تسجيلك بنجاح'
+
+        return redirect('visitor_register',eventuuid)
+
+    return render(request,'visitor-register.html',context)
